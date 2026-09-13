ad971ccaee696fe817d7b904cac844e1b1778226
Yogesh B N <bnyogesh13@gmail.com>
2026-09-13 12:05:50 +0530

    Add user registration and login with JWT

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const Problem = require('./models/Problem')
+const authRoutes = require('./routes/auth')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/auth', authRoutes)
 
-mongoose.connect(process.env.MONGO_URI)
+ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err))
 
